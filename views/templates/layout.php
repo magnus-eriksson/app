9a8231a57ffd07b11e11041f79ab808352ca637f
Magnus Eriksson <mange@reloop.se>
2023-10-30 13:17:45 +0100

    Updated

--- views/templates/layout.php
+++ views/templates/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moments</title>
+    <title>App</title>
     <link rel="stylesheet" type="text/css" href="/static/main.css" />
 </head>
 
